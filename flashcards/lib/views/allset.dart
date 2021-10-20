8486a646d227529e21a3383c129cdb83286a8611
chowdhuryrahulc <84246818+chowdhuryrahulc@users.noreply.github.com>
2021-10-20 18:17:59 +0530

    Big Commit

--- flashcards/lib/views/allset.dart
+++ flashcards/lib/views/allset.dart
@@ -5,7 +5,7 @@ import 'package:flashcards/reviewpractice.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
 import '../drawer.dart';
-import '../floatingdialog.dart';
+// import '../floatingdialog.dart';
 
 bool LoggedIn = false;
 GoogleSignInAccount? A;
@@ -13,8 +13,6 @@ GoogleSignIn sign = GoogleSignIn();
 title? titl;
 
 class Firstpage extends StatefulWidget {
-  // static var dark;
-
   const Firstpage({Key? key}) : super(key: key);
 
   @override
@@ -61,13 +59,15 @@ class _FirstpageState extends State<Firstpage> {
       drawer: drawer(context),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: reviewpractice(litems: litems),
+        child: reviewpractice(),
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: Text("CREATE SET"),
         icon: Icon(Icons.add),
         onPressed: () {
-          floatingdialog(context);
+          floatingdialog(context).then((val) {
+            setState(() {});
+          });
         },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
@@ -115,5 +115,130 @@ class _FirstpageState extends State<Firstpage> {
         );
       },
     );
+  }
+}
+
+@override
+floatingdialog(BuildContext context, {String? title, String? description}) {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  // print(title);
+  if (title != null) {
+    titleController.text = title;
+    // descriptionController.text= description;
+  }
+
+  final _formKey = GlobalKey<FormState>();
+
+  String? Z;
+  String? M;
+
+  const Y = <String>[
+    'English',
+    'Arabic',
+    'Bangla',
+    'Bosnian',
+    'Catalan',
+    'Czech'
+  ];
+  final List<DropdownMenuItem<String>> J = Y
+      .map(
+        (String value) => DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        ),
+      )
+      .toList();
+
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(builder: (context, setState) {
+          return Form(
+            key: _formKey,
+            child: SimpleDialog(
+              //?title,content
+              contentPadding: EdgeInsets.all(10),
+              children: [
+                TextFormField(
+                  validator: (val) =>
+                      val!.isNotEmpty ? null : 'Name Should Not Be Empty',
+                  controller: titleController,
+                  decoration: InputDecoration(
+                      labelText: "Name",
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0)))),
+                ),
+                TextField(
+                  controller: descriptionController,
+                  decoration: InputDecoration(
+                      labelText: "Description-optional",
+                      border: OutlineInputBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(10.0)))),
+                ),
+                Row(
+                  children: [
+                    Text("Category"),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.access_alarm),
+                    )
+                  ],
+                ),
+                Text("Term language"),
+                DropdownButton<String>(
+                  //!drop down
+                  value: Z,
+                  hint: Text('English'),
+                  onChanged: (String? N) {
+                    if (N != null) {
+                      Z = N;
+                    }
+                  },
+                  items: J,
+                ),
+                Text("Definition language"),
+                DropdownButton<String>(
+                  value: M,
+                  hint: Text('English'),
+                  onChanged: (String? N1) {
+                    if (N1 != null) {
+                      M = N1;
+                    }
+                  },
+                  items: J,
+                ),
+                Text(
+                    "In case of missing languages, or if audio does not work you need to install the language."),
+                TextButton(onPressed: () {}, child: Text("INSTALL LANGUAGES")),
+                FloatingActionButton.extended(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _submitTitle(context, titleController.text,
+                            descriptionController.text);
+                        setState(() {
+                          Navigator.pop(context);
+                        });
+                      }
+                    },
+                    label: Text("ADD CARDS")),
+              ],
+            ),
+          );
+        });
+      });
+}
+
+_submitTitle(BuildContext context, ttleControl, descripControl) {
+  final DBManager dbManager = DBManager();
+
+  if (titl == null) {
+    title ttl = title(name: ttleControl, description: descripControl);
+    dbManager.insertTitle(ttl).then((value) => null);
+  } else {
+    title ttl = title(name: ttleControl, description: descripControl);
+    dbManager.updateTitle(ttl);
   }
 }
