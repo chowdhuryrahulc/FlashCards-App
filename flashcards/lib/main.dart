1c73a35b062d897920b08f1db363d0f5f395dac7
chowdhuryrahulc <84246818+chowdhuryrahulc@users.noreply.github.com>
2021-10-27 08:02:48 +0530

    New2

--- flashcards/lib/main.dart
+++ flashcards/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flashcards/Profilepage.dart';
-import 'package:flashcards/google.dart';
+import 'package:flashcards/views/google.dart';
 import 'package:flashcards/database/google_sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:flashcards/views/Firstpage.dart';
--- flashcards/lib/views/google.dart
+++ flashcards/lib/views/google.dart
@@ -1,12 +1,9 @@
-// import 'dart:html';
-
 import 'package:flashcards/database/google_sign_in.dart';
 import 'package:flashcards/views/Firstpage.dart';
 import 'package:flashcards/main.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class google extends StatefulWidget {
@@ -17,9 +14,6 @@ class google extends StatefulWidget {
 }
 
 class _googleState extends State<google> {
-  // GoogleSignInAccount? googleAccount;
-  // var googleSignIn = GoogleSignIn();
-
   @override
   void initState() {
     super.initState();
@@ -38,20 +32,3 @@ class _googleState extends State<google> {
     ));
   }
 }
-
-// class GoogleSignInProvider extends ChangeNotifier {
-//   final googleSignIn = GoogleSignIn();
-//   GoogleSignInAccount? user;
-//   Future googleLogin() async {
-//     final googleUser = await googleSignIn.signIn();
-//     if (googleUser == null) return;
-//     user = googleUser;
-//     final googleAuth = await googleUser.authentication;
-//     final credential = GoogleSignInProvider.credential(
-//       accwssToken: googleAuth.accessToken,
-//       idToken: googleAuth.idToken,
-//     );
-//     await FirebaseAuth.instance.signInWithCredential(credential);
-//     notifyListeners();
-//   }
-// }
