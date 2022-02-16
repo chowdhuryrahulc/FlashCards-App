677e26bbe568e7c353c3e9579f240836a2a0f558
chowdhuryrahulc <84246818+chowdhuryrahulc@users.noreply.github.com>
2022-02-16 22:04:37 +0530

    Nothing

--- flashcards/lib/views/grid_view.dart
+++ flashcards/lib/views/grid_view.dart
@@ -27,123 +27,25 @@ class _gridViewState extends State<gridView> {
 
   bool visibleListExample = false;
 
-  CardGridX(BuildContext context, VocabCardModal list) {
-    final VocabDatabase dbManager2 = VocabDatabase();
-
-    // updateFavoriteTitle(int favoriteToggle) {
-    //   if (favoriteToggle == 0) {
-    //     setState(() {
-    //       dbManager2.updateFavoriteTitle(list, 1);
-    //     });
-    //   } else if (favoriteToggle == 1) {
-    //     setState(() {
-    //       dbManager2.updateFavoriteTitle(list, 0);
-    //     });
-    //   }
-    // }
-
-    return Container(
-      color: Theme.of(context).colorScheme.secondary,
-      margin: EdgeInsets.all(4.0),
-      padding: EdgeInsets.only(top: 8.0, left: 8.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            list.term,
-            style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Theme.of(context).colorScheme.primary),
-          ),
-          SizedBox(height: 10),
-          Text(list.defination,
-              style: TextStyle(
-                  fontSize: 15, color: Theme.of(context).colorScheme.primary)),
-          SizedBox(height: 15),
-          Text(list.example!,
-              style: TextStyle(
-                  fontSize: 12,
-                  fontStyle: FontStyle.italic,
-                  color: Theme.of(context).colorScheme.primary)),
-          list.picture != null
-              ? Center(
-                  child: Container(
-                      height: 100,
-                      color: Colors.white,
-                      child: Image.memory(list.picture!)))
-              : SizedBox(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconButton(
-                color: Colors.red,
-                onPressed: () {
-                  // updateFavoriteTitle(list.favorite ?? 0);
-                  context
-                      .read<gridViewVisibleControl>()
-                      .updateFavoriteTitle(list.favorite ?? 0, list);
-                },
-                icon:
-                    context.watch<gridViewVisibleControl>().favoriteToggle == 1
-                        ? Icon(Icons.favorite)
-                        : Icon(Icons.favorite_border),
-              ),
-              PopupMenuButton(itemBuilder: (BuildContext context) {
-                return [
-                  PopupMenuItem(
-                      child: InkWell(
-                          onTap: () {
-                            bool? edi = true;
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => write(
-                                          editxyz: edi,
-                                          termxyz: list.term,
-                                          definationxyz: list.defination,
-                                          examplexyz: list.example,
-                                          vocabCard: list,
-                                          currentSet: widget
-                                              .currentSetUsedForDatabaseSearch,
-                                        ))).then((value) {
-                              setState(() {
-                                Navigator.pop(context);
-                              });
-                            });
-                          },
-                          child: popUpTitle(Icons.edit, 'Edit'))),
-                  PopupMenuItem(
-                      child: popUpTitle(Icons.show_chart, 'Progress')),
-                  PopupMenuItem(
-                      child: popUpTitle(
-                          Icons.add_circle_outline, 'Add to ignored')),
-                  PopupMenuItem(
-                      child: InkWell(
-                    onTap: () {
-                      dbManager2
-                          .deleteTitle(list.nd_id!)
-                          .then((value) => setState(() {
-                                Navigator.pop(context);
-                              }));
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Icon(Icons.delete, color: Colors.red),
-                        Text(' Remove')
-                      ],
-                    ),
-                  ))
-                ];
-              }),
-            ],
-          )
-        ],
-      ),
-    );
+  updateFavoriteTitle(
+    int favoriteToggle,
+    VocabCardModal singleVocabCard,
+  ) {
+    if (favoriteToggle == 0) {
+      setState(() {
+        dbManager2.updateFavoriteTitle(singleVocabCard, 1);
+      });
+    } else if (favoriteToggle == 1) {
+      setState(() {
+        dbManager2.updateFavoriteTitle(singleVocabCard, 0);
+      });
+    }
   }
+
+  // CardGridX(context, VocabCardModal singleVocabCard) {
+  //   final VocabDatabase dbManager2 = VocabDatabase();
+
+  //   return }
 
   @override
   Widget build(BuildContext context) {
@@ -179,14 +81,139 @@ class _gridViewState extends State<gridView> {
                   child: StaggeredGridView.countBuilder(
                     crossAxisCount: 2,
                     itemCount: widget.vocabCardModalList.length,
-                    itemBuilder: (context, index) {
-                      return CardGridX(
-                          context, widget.vocabCardModalList[index]);
-                    },
                     staggeredTileBuilder: (index) => StaggeredTile.fit(1),
                     mainAxisSpacing: 2.0,
                     crossAxisSpacing: 2.0,
                     shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      VocabCardModal singleVocabCard =
+                          widget.vocabCardModalList[index];
+                      return Container(
+                        color: Theme.of(context).colorScheme.secondary,
+                        margin: EdgeInsets.all(4.0),
+                        padding: EdgeInsets.only(top: 8.0, left: 8.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              singleVocabCard.term,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Theme.of(context).colorScheme.primary),
+                            ),
+                            SizedBox(height: 10),
+                            Text(singleVocabCard.defination,
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color:
+                                        Theme.of(context).colorScheme.primary)),
+                            SizedBox(height: 15),
+                            Text(singleVocabCard.example!,
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    fontStyle: FontStyle.italic,
+                                    color:
+                                        Theme.of(context).colorScheme.primary)),
+                            singleVocabCard.picture != null
+                                ? Center(
+                                    child: Container(
+                                        height: 100,
+                                        color: Colors.white,
+                                        child: Image.memory(
+                                            singleVocabCard.picture!)))
+                                : SizedBox(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                  color: Colors.red,
+                                  onPressed: () {
+                                    updateFavoriteTitle(
+                                        singleVocabCard.favorite ?? 0,
+                                        singleVocabCard);
+                                    // context.read<gridViewVisibleControl>().updateFavoriteTitle(
+                                    //     singleVocabCard.favorite ?? 0, singleVocabCard);
+                                  },
+                                  icon: Icon(() {
+                                    if (singleVocabCard.favorite == 1) {
+                                      return Icons.favorite;
+                                    } else {
+                                      return Icons.favorite_border;
+                                    }
+                                  }()),
+                                  // context.watch<gridViewVisibleControl>().favoriteToggle == 1
+                                  //     ? Icon(Icons.favorite)
+                                  //     : Icon(Icons.favorite_border),
+                                ),
+                                PopupMenuButton(
+                                    itemBuilder: (BuildContext context) {
+                                  return [
+                                    PopupMenuItem(
+                                        child: InkWell(
+                                            onTap: () {
+                                              bool? edi = true;
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          write(
+                                                            editxyz: edi,
+                                                            termxyz:
+                                                                singleVocabCard
+                                                                    .term,
+                                                            definationxyz:
+                                                                singleVocabCard
+                                                                    .defination,
+                                                            examplexyz:
+                                                                singleVocabCard
+                                                                    .example,
+                                                            vocabCard:
+                                                                singleVocabCard,
+                                                            currentSet: widget
+                                                                .currentSetUsedForDatabaseSearch,
+                                                          ))).then((value) {
+                                                setState(() {
+                                                  Navigator.pop(context);
+                                                });
+                                              });
+                                            },
+                                            child: popUpTitle(
+                                                Icons.edit, 'Edit'))),
+                                    PopupMenuItem(
+                                        child: popUpTitle(
+                                            Icons.show_chart, 'Progress')),
+                                    PopupMenuItem(
+                                        child: popUpTitle(
+                                            Icons.add_circle_outline,
+                                            'Add to ignored')),
+                                    PopupMenuItem(
+                                        child: InkWell(
+                                      onTap: () {
+                                        dbManager2
+                                            .deleteTitle(singleVocabCard.nd_id!)
+                                            .then((value) => setState(() {
+                                                  Navigator.pop(context);
+                                                }));
+                                      },
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Icon(Icons.delete, color: Colors.red),
+                                          Text(' Remove')
+                                        ],
+                                      ),
+                                    ))
+                                  ];
+                                }),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
                   ),
                 )
               : gridViewEmptyContainer()),
